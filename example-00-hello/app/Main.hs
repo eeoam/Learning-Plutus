f6e0aed9ca7737eb34a321301c68f3a72687293c
eeoam <eric@emurgo.io>
2022-09-09 16:35:47 +0100

    Simplified example-00

--- example-00-hello/app/Main.hs
+++ example-00-hello/app/Main.hs
@@ -1,17 +1,15 @@
 module Main where
 
-import Data.Functor (void)
+import Control.Monad.Freer.Extras (logInfo)
 import Data.Default(def)
 import Plutus.Trace.Emulator (
     EmulatorTrace,
-    activateContractWallet, waitNSlots, runEmulatorTraceIO')
-import Plutus.Contract.Trace (knownWallet)
-import Hello.OffChain (hello)
+    runEmulatorTraceIO')
+
+
 
 trace :: EmulatorTrace ()
-trace = do
-    void $ activateContractWallet (knownWallet 1) hello
-    void $ waitNSlots 2
+trace = logInfo @String "Hello!"
 
 main :: IO ()
 main = runEmulatorTraceIO' def def trace
