7a8a1caa56e9dc238980936c6627296e1f5dd7da
eeoam <eric@emurgo.io>
2022-09-12 10:44:29 +0100

    Simplified Main.hs.

--- example-01-send-ada/app/Main.hs
+++ example-01-send-ada/app/Main.hs
@@ -1,20 +1,15 @@
-{-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-
 module Main where
 
-import Data.Map qualified as Map
 import Data.Functor (void)
 import Control.Monad.Freer.Extras (logInfo)
 import Data.Default(def)
-import Ledger.Value (Value, AssetClass (..), assetClassValue)
 import Ledger.Ada (lovelaceValueOf)
 import Plutus.Trace.Emulator (
     EmulatorTrace,
     payToWallet,
     runEmulatorTraceIO')
 import Plutus.Contract.Test (w1, w2)
-import SendAda.OffChain (sendAda)
+
 
 trace :: EmulatorTrace ()
 trace = void $ payToWallet w1 w2 $ lovelaceValueOf 1_000_000
