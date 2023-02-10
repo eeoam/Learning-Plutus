af986b4940fb04d850472e5ed1aff0a18e891840
eeoam <eric@emurgo.io>
2023-02-10 17:48:55 +0000

    Added send-ada and hello-contract examples.

---
+++ basics/app/hello-contract.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Prelude (($))
+import Prelude qualified as Prel
+import Data.Text (Text)
+import Data.Functor qualified as Functor
+import Data.Default qualified as Default
+import Control.Monad.Freer.Extras qualified as Extras
+import Ledger.Ada qualified as Ada
+import Plutus.Trace.Emulator qualified as Trace
+import Plutus.Contract.Test qualified as Test
+import Plutus.Contract as Contract
+
+main :: Prel.IO ()
+main = Trace.runEmulatorTraceIO' Default.def Default.def trace
+    where 
+        trace :: Trace.EmulatorTrace ()
+        trace = Functor.void $ do
+                    Extras.logInfo @Prel.String "Running contract..."
+                    Trace.activateContractWallet Test.w1 hello
+
+        hello :: Contract () Empty Text ()
+        hello = do
+            Contract.logInfo @Prel.String "Hello from contract!"
